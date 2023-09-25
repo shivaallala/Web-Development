d80219a43cb0f8873a59b43b229e20f92ba78c96
shivaallala <shivaallala@gmail.com>
2023-09-25 13:52:48 -0400

    Update

--- Testing/Testing.js
+++ Testing/Testing.js
@@ -1,15 +1,36 @@
 function factorial(n) {
-    if (n===0) {
-        return 1;
-    } else {
-        return n * factorial(n-1);
-    }
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+
+  // return n === 0 ? 1 : n * factorial(n - 1);
+
+
+
 }
 
 function calculateFactorial() {
-    const number = 9;
-    const result = factorial(number);
-    console.log(`The factorial of ${number} is ${result}`);
-    const calculateButton = document.getElementById("calculateButton");
-    calculateButton.textContent = `Factorial of ${number} is ${result}`;
+  const number = 9;
+  const result = factorial(number);
+  console.log(`The factorial of ${number} is ${result}`);
+  const calculateButton = document.getElementById("calculateButton");
+  calculateButton.textContent = `Factorial of ${number} is ${result}`;
+}
+
+const countElement = document.getElementById("count");
+
+function setCount() {
+  let count = Number(countElement.textContent);
+  count = count + 1;
+  countElement.textContent = count;
+}
+
+function powerOf(number, power) {
+  if (power === 0) {
+    return 1;
+  } else {
+    return number * powerOf(number, power - 1);
+  }
 }
---
+++ react1/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <h1></h1>
+    )
+}
+
+export default App;
